6872fc9489fae3b3c9ee92788d9d2d8f581dcda0
seker212 <seker212@gmail.com>
2021-10-15 19:01:41 +0200

    update 01

--- Lab01/Lab01_run.py
+++ Lab01/Lab01_run.py
@@ -3,8 +3,8 @@ import subprocess
 import re
 from types import prepare_class
 
-def runLab1(filePath, max_threads_num):
-    with open('lab01.txt', 'w+') as output:
+def runLab1(filePath, max_threads_num, output_file):
+    with open(output_file, 'w+') as output:
         for i in range(1, max_threads_num+1):
             print(f"start: {i}")
             process = subprocess.run([filePath, str(i)], capture_output=True, encoding='utf-8')
@@ -14,5 +14,7 @@ def runLab1(filePath, max_threads_num):
             print(f"\n========= {i} =========\n" + process.stdout + "\n\n")
 
 if __name__ == '__main__':
-    # runLab1('.\\x64\\Release\\Lab01.exe', 24)
-    runLab1('.\\Debug\\Lab01.exe', 24)
+    runLab1('..\\x64\\Release\\Lab01.exe', 24, 'r64.txt')
+    runLab1('..\\x64\\Debug\\Lab01.exe', 24, 'd64.txt')
+    runLab1('..\\Debug\\Lab01.exe', 24, 'd32.txt')
+    runLab1('..\\Release\\Lab01.exe', 24, 'r32.txt')
